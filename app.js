873cf42c4977fe7a11083ec999380e707760910c
Bolade <ottolaharris@yahoo.fr>
2017-10-14 20:06:24 -0400

    no errors

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ connection.connect(function(err) {
         console.log("Error connecting database");
     }
 });*/
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "teddybravoç&",
+	database: "test"
+});
+connection.connect(function(err) {
+    if (err) {
+        console.log(err);
+        console.log("Error connecting database");
+    }
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
@@ -19,6 +33,31 @@ app.use(bodyParser.urlencoded({
 
 app.get("/", function(req, res){
   res.render("index");
+});
+
+app.get("/sub", function(req, res){
+	res.render("submit.ejs");
+});
+
+app.post("/insert", function(req, res){
+	console.log(req.body);
+	console.log(req.files);
+	var data={
+		name: req.body.nom,
+		telephone: req.body.tel,
+		description: req.body.des,
+		cv: req.body.cv,
+		img: req.body.pic
+	};
+	console.log(data);
+	var query="insert into jobber set ? ";
+	connection.query(query, data, function (err, result) {
+	    if (err) 
+	    	throw err;
+	    console.log("1 record inserted");
+	});
+	console.log("name = "+req.body.nom);
+	res.redirect("/sub");
 });
 
 app.listen(port, function() {
